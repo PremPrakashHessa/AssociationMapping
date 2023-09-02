98b248e5e4f8ed65e25696f0e242ecd525e57690
Prem_Prakash <premprakash8721@gmail.com>
2023-09-02 17:01:00 +0530

    Mapping Set = new HashSet() , in db in collection table we have combined PK to ensure no duplicates are allowed for Set

--- src/main/java/com/example/associationmapping/entities/Item.java
+++ src/main/java/com/example/associationmapping/entities/Item.java
@@ -1,5 +1,27 @@
 package com.example.associationmapping.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
 public class Item {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "ImagePath",
+            joinColumns = @JoinColumn(name = "Item_id")
+    )
+    @Column(name = "Path")
+    private Set<String> imagePaths = new HashSet<>();
 
 }
---
+++ src/main/java/com/example/associationmapping/repositories/ItemRepository.java
@@ -0,0 +1,18 @@
+package com.example.associationmapping.repositories;
+
+import com.example.associationmapping.entities.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select i from Item i inner join fetch i.imagePaths where i.id = :id")
+    Item findWithImages(@Param("id") Long id);
+
+    @Query(value = "SELECT PATH FROM IMAGEPATH WHERE ITEM_ID = ?1" , nativeQuery = true)
+    Set<String> getImagePathsOfItem(Long id);
+}
